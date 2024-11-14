5a7901ea2c5a326b13feca0989f7aad6f487a0b7
Fly-PD <165564689+Fly-PD@users.noreply.github.com>
2024-11-14 23:11:30 +0100

    Update cryptoLib.py
    aggiunto commenti e funzione di replace

--- cryptoLib.py
+++ cryptoLib.py
@@ -4,6 +4,7 @@ from textwrap import wrap
 asciiChar = [chr(i).encode("ascii") for i in range(0, 128)]
 
 
+# Shift char in message by pos
 def ROT(message, pos, alphabet=list(string.printable)):
     rot13_enc = ''
     for c in message:
@@ -12,6 +13,7 @@ def ROT(message, pos, alphabet=list(string.printable)):
     return rot13_enc
 
 
+# XOR the message with the given key
 def XOR(message, KEY):
     rep = len(message)//len(KEY) + 1
     key = (KEY*rep)[:len(message)] # adjust the key len
@@ -19,7 +21,8 @@ def XOR(message, KEY):
     return xored
 
 
-
+# Convert string with only 0 and 1 to the corrisponding string
+# if there is a separator between bytes specify it
 def binToStr(binStr: str, separator: str =None):
     if separator == None:
         binStr = wrap(binStr, 7)
@@ -29,3 +32,11 @@ def binToStr(binStr: str, separator: str =None):
     for char in binStr:
         message += chr(int(char, 2))
     return message
+
+
+# Replace all char in message with the corrisponding specified in charList
+def replacer(message: str, charList: dict):
+    msg = message
+    for key in charList:
+        msg = msg.replace(key, charList[key])
+    return msg
